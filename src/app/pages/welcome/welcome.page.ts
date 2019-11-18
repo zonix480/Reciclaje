524cbcbae5c5e0a2a7253e7303cf3be43e33d7b2
Julian Cruz <juliancruz300@gmail.com>
2019-11-18 16:38:51 -0500

    Fixed evaluated rol

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GlobalProvider} from '../../../providers/global';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
-
-  constructor() { }
+  public rol;
+  constructor(private globalProv:GlobalProvider) { }
 
   ngOnInit() {
+    this.globalProv.getStorage("user").then(res => {
+      this.rol = res.rols_id;   
+    })
   }
 
 }
